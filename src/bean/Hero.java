7b6c3ca8f0bf58bf08da27b652e4e49f7f33e511
ChenBo <2235048462@qq.com>
2018-10-31 18:13:50 +0800

    Initial commit

--- src/bean/Hero.java
+++ src/bean/Hero.java
@@ -37,4 +37,9 @@ public class Hero {
     public void setDamage ( int damage ) {
         this.damage = damage;
     }
+
+    @Override
+    public String toString () {
+        return "Hero [name=" + name + ",hp=" + hp + ",damage=" + damage + "]";
+    }
 }
